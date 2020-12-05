4ef6c28f15a73fa819d309226c369c54548ea430
Patrick Khouri <patrick.darya@gmail.com>
2020-12-05 13:47:09 -0300

    created the person model and validators

---
+++ app/models/person.rb
@@ -0,0 +1,10 @@
+class Person < ApplicationRecord
+  validates :age, presence: true
+  validates :dependents, presence: true
+  validates :income, presence: true
+  validates :marital_status, presence: true
+  validates :house, :vehicle, :risk_question_1, :risk_question_2, :risk_question_3, inclusion: { in: [true, false]}
+  validates :marital_status, inclusion: { in: ['single', 'married'] }
+  validates :ownership_status, inclusion: { in: ['owned', 'mortgaged']}
+  validates :age, :dependents, :income, :numericality => { greater_than_or_equal_to: 0}
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Person.destroy_all
+
+puts 'Creating Person...'
+
+test = Person.create!(age: 25, dependents: 0, house: false, ownership_status: 'owned', income: 20000, marital_status: 'single', 
+risk_question_1: false, risk_question_2: true, risk_question_3: false,  vehicle: true, year: 2019) 
+
+puts 'Person created!!'
