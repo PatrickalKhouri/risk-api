482ac71249c86be0a6724fc1fd261f082547795a
Patrick Khouri <patrick.darya@gmail.com>
2020-12-07 13:48:42 -0300

    added post request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get '/risk-result', to: 'persons#risk_calculation'
   post '/risk-evaluation', to: 'persons#create'
 end
